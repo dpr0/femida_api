843f6ce606a666e1f30de6b792144133565ad42f
dvitvitskiy <dmitriy.vitvitskiy@sbermarket.ru>
2023-07-02 21:19:32 +0300

    master2

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ server '80.68.7.92', port: 22, roles: %w(app db web), primary: true
 
 set :rbenv_ruby,      '3.0.5'
 set :application,     'ruby_pub'
-set :repo_url,        'git@github.com:khakanabulov/ruby_pub.git'
+set :repo_url,        'gitlab.com:femida1/ruby_pub.git'
 set :linked_files,    fetch(:linked_files, []).push('config/cable.yml', 'config/database.yml', 'config/secrets.yml', 'config/master.key', 'config/credentials.yml.enc', '.env')
 set :linked_dirs,     fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/session_store', 'vendor/bundle', 'public/system', 'public/uploads')
 set :keep_releases,   5
