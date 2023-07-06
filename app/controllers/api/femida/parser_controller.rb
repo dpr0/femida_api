9cf47adc1264bddba9b1413a21246e5489c4e6a8
dvitvitskiy <dmitriy.vitvitskiy@sbermarket.ru>
2023-07-06 10:01:34 +0300

    leaks

--- app/controllers/api/femida/parser_controller.rb
+++ app/controllers/api/femida/parser_controller.rb
@@ -137,26 +137,26 @@ class Api::Femida::ParserController < ApplicationController
     #   end
     # end
 
-    # TurbozaimUser.where(is_phone_verified: ['false', nil]).each do |u|
-    #   f = u.phone.last(10)
-    #   users = ParsedUser.where(phone: ["7#{f}", f])
-    #
-    #   bool = users.select { |user| user.last_name.downcase == u.last_name.downcase && user.first_name.downcase == u.first_name.downcase }.present?
-    #   puts "=================================================== #{bool}"
-    #   u.update(os_status: bool)
-    # end
-
     TurbozaimUser.where(is_phone_verified: ['false', nil]).each do |u|
       f = u.phone.last(10)
-      # 5 users = Leaks1.where('Telephone' => ["7#{f}", f])
-      users = Leaks8.where(phone: ["7#{f}", f])
+      users = ParsedUser.where(phone: ["7#{f}", f])
 
-      # bool = users.select { |user| user.surname.downcase == u.last_name.downcase && user.name.downcase == u.first_name.downcase }.present?
-      # bool = users.select { |user| user.LastName.downcase == u.last_name.downcase && user.FirstName.downcase == u.first_name.downcase }.present?
-      bool = users.select { |user| user.surname&.downcase == u.last_name.downcase && user.name&.downcase == u.first_name.downcase }.present?
+      bool = users.select { |user| user.last_name&.downcase == u.last_name.downcase && user.first_name&.downcase == u.first_name.downcase }.present?
       puts '==================================================== ' if bool
       u.update(os_status: bool) if bool
     end
+
+    # TurbozaimUser.where(is_phone_verified: ['false', nil]).each do |u|
+    #   f = u.phone.last(10)
+    #   # 5 users = Leaks1.where('Telephone' => ["7#{f}", f])
+    #   users = Leaks8.where(phone: ["7#{f}", f])
+    #
+    #   # bool = users.select { |user| user.surname.downcase == u.last_name.downcase && user.name.downcase == u.first_name.downcase }.present?
+    #   # bool = users.select { |user| user.LastName.downcase == u.last_name.downcase && user.FirstName.downcase == u.first_name.downcase }.present?
+    #   bool = users.select { |user| user.surname&.downcase == u.last_name.downcase && user.name&.downcase == u.first_name.downcase }.present?
+    #   puts '==================================================== ' if bool
+    #   u.update(os_status: bool) if bool
+    # end
   end
 
   def narod
