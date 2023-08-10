47430613a39bc2998036d98dae786cbcb0db7e4b
dvitvitskiy <dvitvitskiy.pro@gmail.com>
2023-08-10 23:51:30 +0300

    okb

--- app/controllers/api/femida/okb_controller.rb
+++ app/controllers/api/femida/okb_controller.rb
@@ -10,47 +10,50 @@ class Api::Femida::OkbController < ApplicationController
     # with_error_handling do
       # #{PATH}certmgr -list -store umy
       # #{PATH}certmgr -list -store uroot
-      curl = "#{PATH}curl -i -X POST -vvv --cert #{ENV['CERT_SHA1_THUMBPRINT']}:#{ENV['CERT_PASSWORD']} \\
-              --cert-type CERT_SHA1_HASH_PROP_ID:CERT_SYSTEM_STORE_CURRENT_USER:MY"
-      hash = {
-        client_id:      ENV['OKB_CLIENT_ID'],
-        client_secret:  ENV['OKB_CLIENT_SECRET'],
-        grant_type:    'client_credentials',
-        scope:         'openid'
-      }
+    curl = "#{PATH}curl -i -X POST -vvv --cert #{ENV['CERT_SHA1_THUMBPRINT']}:#{ENV['CERT_PASSWORD']} \\
+            --cert-type CERT_SHA1_HASH_PROP_ID:CERT_SYSTEM_STORE_CURRENT_USER:MY"
+    Rails.logger.info('curl:')
+    Rails.logger.info(curl)
+
+    hash = {
+      client_id:      ENV['OKB_CLIENT_ID'],
+      client_secret:  ENV['OKB_CLIENT_SECRET'],
+      grant_type:    'client_credentials',
+      scope:         'openid'
+    }
 
     Rails.logger.info('hash_map:')
     Rails.logger.info(hash.map { |key, value| "-d #{key}=#{value} \\" }.join(' '))
 
     auth = parse_json `#{curl} -H Content-Type:application/x-www-form-urlencoded \\
-        #{hash.map { |key, value| "-d #{key}=#{value} \\" }.join(' ')} #{ENV['OKB_HOST']}auth`
+      #{hash.map { |key, value| "-d #{key}=#{value} \\" }.join(' ')} #{ENV['OKB_HOST']}auth`
 
-      json = {
-        submission: {
-          identifier: '1',
-          date: '2022-12-18',
-          app_date: '2022-12-18'
-        },
-        applicant: {
-          name: 'ХАНТЕР',
-          surname: 'ХАНТЕР',
-          patronymic: 'ХАНТЕР',
-          birthday: '1988-01-09',
-          consent: 'Y',
-          document: 1655054567,
-          issued_at: '2022-01-23',
-          telephone_number: '+79031234567'
-        }
-      }.to_json
+    json = {
+      submission: {
+        identifier: '1',
+        date: '2022-12-18',
+        app_date: '2022-12-18'
+      },
+      applicant: {
+        name: 'ХАНТЕР',
+        surname: 'ХАНТЕР',
+        patronymic: 'ХАНТЕР',
+        birthday: '1988-01-09',
+        consent: 'Y',
+        document: 1655054567,
+        issued_at: '2022-01-23',
+        telephone_number: '+79031234567'
+      }
+    }.to_json
     Rails.logger.info('json:')
     Rails.logger.info(json)
 
     resp = parse_json `#{curl} -H Content-Type:application/json \\
-        -H "Authorization:Bearer #{auth['access_token']}" \\
-        -H "X-Request-Id:#{ENV['OKB_CLIENT_SECRET']}" \\
-        -d '#{json}' \\
-        #{ENV['OKB_HOST']}verify
-      `
+      -H "Authorization:Bearer #{auth['access_token']}" \\
+      -H "X-Request-Id:#{ENV['OKB_CLIENT_SECRET']}" \\
+      -d '#{json}' \\
+      #{ENV['OKB_HOST']}verify
+    `
     render status: :ok, json: resp
     # end
   end
