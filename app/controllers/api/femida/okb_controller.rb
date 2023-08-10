fa2e3bdf3d28c66055ea152ed3d73d2c7903578e
dvitvitskiy <dvitvitskiy.pro@gmail.com>
2023-08-10 23:41:05 +0300

    okb

--- app/controllers/api/femida/okb_controller.rb
+++ app/controllers/api/femida/okb_controller.rb
@@ -5,6 +5,7 @@ class Api::Femida::OkbController < ApplicationController
 
   PATH = '/opt/cprocsp/bin/amd64/'
   HOST = 'https://idv-tst.bki-okb.com/'
+  CLIENT_ID = 'ucb.client.6529_generic'
   CLIENT_SECRET = 'c2087e61-2c6c-4ed9-ace8-8703ab9f5bda'
 
   api :GET, '/okb/:id', "Проверка ОКБ"
@@ -12,40 +13,39 @@ class Api::Femida::OkbController < ApplicationController
     with_error_handling do
       # #{PATH}certmgr -list -store umy
       # #{PATH}certmgr -list -store uroot
-      curl = "
-        #{PATH}curl -i -X POST -vvv \\
-        --cert-type CERT_SHA1_HASH_PROP_ID:CERT_SYSTEM_STORE_CURRENT_USER:MY \\
-        --cert 2fc4461b7e9705acc998203bf19df8296e252861:123 \\
-      "
+      curl = "#{PATH}curl -i -X POST -vvv --cert #{ENV['CERT_SHA1_THUMBPRINT']}:#{ENV['CERT_PASSWORD']} \\
+              --cert-type CERT_SHA1_HASH_PROP_ID:CERT_SYSTEM_STORE_CURRENT_USER:MY"
+      hash = {
+        client_id:      ENV['OKB_CLIENT_ID'],
+        client_secret:  ENV['OKB_CLIENT_SECRET'],
+        grant_type:    'client_credentials',
+        scope:         'openid'
+      }
       auth = parse_json `#{curl} -H Content-Type:application/x-www-form-urlencoded \\
-        -d client_id=ucb.client.6529_generic \\
-        -d client_secret=#{CLIENT_SECRET} \\
-        -d grant_type=client_credentials \\
-        -d scope=openid \\
-        #{HOST}auth
-      `
+        #{hash.map { |key, value| "-d #{key}=#{value} \\" }.join(' ')} #{ENV['OKB_HOST']}auth`
 
+      json = {
+        submission: {
+          identifier: '1',
+          date: '2022-12-18',
+          app_date: '2022-12-18'
+        },
+        applicant: {
+          name: 'ХАНТЕР',
+          surname: 'ХАНТЕР',
+          patronymic: 'ХАНТЕР',
+          birthday: '1988-01-09',
+          consent: 'Y',
+          document: 1655054567,
+          issued_at: '2022-01-23',
+          telephone_number: '+79031234567'
+        }
+      }.to_json
       parse_json `#{curl} -H Content-Type:application/json \\
         -H "Authorization:Bearer #{auth['access_token']}" \\
         -H "X-Request-Id:#{CLIENT_SECRET}" \\
-        -d '{
-          "submission": {
-            "identifier": "1",
-            "date": "2022-12-18",
-            "app_date": "2022-12-18"
-          },
-          "applicant": {
-            "name": "ХАНТЕР",
-            "surname": "ХАНТЕР",
-            "patronymic": "ХАНТЕР",
-            "birthday": "1988-01-09",
-            "consent": "Y",
-            "document": 1655054567,
-            "issued_at": "2022-01-23",
-            "telephone_number": "+79031234567"
-          }
-        }' \\
-        #{HOST}verify
+        -d '#{json}' \\
+        #{ENV['OKB_HOST']}verify
       `
     end
   end
@@ -54,6 +54,6 @@ class Api::Femida::OkbController < ApplicationController
 
   def parse_json(str)
     Rails.logger.info(str)
-    JSON.parse(str.split("\n\n").last)
+    JSON.parse(str.split("r\n\r\n").last)
   end
 end
