91c3edf064a975953d6d7e9d685092e78115cd21
dvitvitskiy <dvitvitskiy.pro@gmail.com>
2023-08-11 11:20:05 +0300

    okb

--- app/controllers/api/femida/okb_controller.rb
+++ app/controllers/api/femida/okb_controller.rb
@@ -8,11 +8,11 @@ class Api::Femida::OkbController < ApplicationController
     with_error_handling do
       curl = "/opt/cprocsp/bin/amd64/curl -i -X POST -vvv --cert #{ENV['CERT_SHA1_THUMBPRINT']}:#{ENV['CERT_PASSWORD']} --cert-type CERT_SHA1_HASH_PROP_ID:CERT_SYSTEM_STORE_CURRENT_USER:MY"
       hash = { client_id: ENV['OKB_CLIENT_ID'], client_secret: ENV['OKB_CLIENT_SECRET'], grant_type: 'client_credentials', scope: 'openid' }
-      auth = parse_json "#{curl} -H Content-Type:application/x-www-form-urlencoded #{hash_to_str(hash, 'd')} #{ENV['OKB_HOST']}auth"
+      auth = parse_json :auth, "#{curl} -H Content-Type:application/x-www-form-urlencoded #{hash_to_str(hash, 'd')}"
       t = Time.now
-      json = { applicant: params[:okb], submission: { identifier: "#{t.to_i}", date: "#{t.to_date}", app_date: "#{t.to_date}" } }.to_json
+      json = { applicant: params[:okb], submission: { identifier: t.to_i.to_s, date: t.to_date.to_s, app_date: t.to_date.to_s } }.to_json
       hash = { 'Content-Type:': 'application/json', 'Authorization:Bearer ': auth['access_token'], 'X-Request-Id:': ENV['OKB_CLIENT_SECRET'] }
-      parse_json "#{curl} #{hash_to_str(hash, 'H')} -d '#{json}' #{ENV['OKB_HOST']}verify"
+      parse_json :verify, "#{curl} #{hash_to_str(hash, 'H')} -d '#{json}'"
     end
   end
 
@@ -22,10 +22,11 @@ class Api::Femida::OkbController < ApplicationController
     hash.map { |key, value| "-#{letter} \"#{key}#{'=' if letter == 'd'}#{value}\"" }.join(' ')
   end
 
-  def parse_json(str)
-    Rails.logger.info("REQUEST: ============ #{str}")
-    s = "{#{`#{str}`.split('{')[1..].join}"
-    Rails.logger.info("RESPONSE: =========== #{s}")
+  def parse_json(method, str)
+    url = "#{ENV['OKB_HOST']}#{method}"
+    Rails.logger.info("=========== #{method.capitalize} REQUEST: ============ \n #{str} #{url}")
+    s = "{#{`#{str} #{url}`.split('{')[1..].join}"
+    Rails.logger.info("=========== #{method.capitalize} RESPONSE: =========== \n #{s}")
     JSON.parse(s)
   end
 end
