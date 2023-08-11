6dfecb33d52798ddda26c09328ccc55bc9a18eaa
dvitvitskiy <dvitvitskiy.pro@gmail.com>
2023-08-11 10:38:52 +0300

    okb

--- app/controllers/api/femida/okb_controller.rb
+++ app/controllers/api/femida/okb_controller.rb
@@ -3,64 +3,33 @@
 class Api::Femida::OkbController < ApplicationController
   protect_from_forgery with: :null_session
 
-  PATH = '/opt/cprocsp/bin/amd64/'
-
-  api :GET, '/okb/:id', "Проверка ОКБ"
-  def show
-    # with_error_handling do
-      # #{PATH}certmgr -list -store umy
-      # #{PATH}certmgr -list -store uroot
-    curl = "#{PATH}curl -i -X POST -vvv --cert #{ENV['CERT_SHA1_THUMBPRINT']}:#{ENV['CERT_PASSWORD']} --cert-type CERT_SHA1_HASH_PROP_ID:CERT_SYSTEM_STORE_CURRENT_USER:MY"
-    hash = {
-      client_id:      ENV['OKB_CLIENT_ID'],
-      client_secret:  ENV['OKB_CLIENT_SECRET'],
-      grant_type:    'client_credentials',
-      scope:         'openid'
-    }
-    str = "#{curl} -H Content-Type:application/x-www-form-urlencoded #{hash_to_str(hash, 'd')} #{ENV['OKB_HOST']}auth"
-    Rails.logger.info('========================================')
-    Rails.logger.info(str)
-    auth = parse_json '{' + `#{str}`.split("{")[1..-1].join
-
-    json = {
-      submission: {
-        identifier: '1',
-        date: '2022-12-18',
-        app_date: '2022-12-18'
-      },
-      applicant: {
-        name: 'ХАНТЕР',
-        surname: 'ХАНТЕР',
-        patronymic: 'ХАНТЕР',
-        birthday: '1988-01-09',
-        consent: 'Y',
-        document: 1655054567,
-        issued_at: '2022-01-23',
-        telephone_number: '+79031234567'
-      }
-    }.to_json
-    hash = {
-      '"Content-Type:': 'application/json"',
-      '"Authorization:Bearer ': auth['access_token'] + '"',
-      '"X-Request-Id:': ENV['OKB_CLIENT_SECRET'] + '"'
-    }
-
-    str = "#{curl} #{hash_to_str(hash, 'H')} -d '#{json}' #{ENV['OKB_HOST']}verify"
-    Rails.logger.info('========================================')
-    Rails.logger.info(str)
-    resp = parse_json '{' + `#{str}`.split("{")[1..-1].join
-    render status: :ok, json: resp
-    # end
+  api :POST, '/okb', 'Проверка ОКБ'
+  def create # #{PATH}certmgr -list -store umy     # uroot
+    with_error_handling do
+      curl = "/opt/cprocsp/bin/amd64/curl -i -X POST -vvv --cert #{ENV['CERT_SHA1_THUMBPRINT']}:#{ENV['CERT_PASSWORD']} --cert-type CERT_SHA1_HASH_PROP_ID:CERT_SYSTEM_STORE_CURRENT_USER:MY"
+      hash = { client_id: ENV['OKB_CLIENT_ID'], client_secret: ENV['OKB_CLIENT_SECRET'], grant_type: 'client_credentials', scope: 'openid' }
+      str = "#{curl} -H Content-Type:application/x-www-form-urlencoded #{hash_to_str(hash, 'd')} #{ENV['OKB_HOST']}auth"
+      auth = parse_json str
+      t = Time.now
+      json = { applicant: params[:okb], submission: { identifier: "#{t.to_i}", date: "#{t.to_date}", app_date: "#{t.to_date}" } }.to_json
+      hash = { 'Content-Type:': 'application/json', 'Authorization:Bearer ': auth['access_token'], 'X-Request-Id:': ENV['OKB_CLIENT_SECRET'] }
+      str = "#{curl} #{hash_to_str(hash, 'H')} -d '#{json}' #{ENV['OKB_HOST']}verify"
+      parse_json str
+    end
   end
 
   private
 
   def hash_to_str(hash, letter)
-    hash.map { |key, value| "-#{letter} #{key}#{letter == 'd' ? '=' : ''}#{value}" }.join(' ')
+    hash.map { |key, value| "-#{letter} \"#{key}#{'=' if letter == 'd'}#{value}\"" }.join(' ')
   end
 
   def parse_json(str)
+    Rails.logger.info('REQUEST: ========================================')
     Rails.logger.info(str)
-    JSON.parse(str)
+    s = '{' + `#{str}`.split("{")[1..-1].join
+    Rails.logger.info('RESPONSE: =======================================')
+    Rails.logger.info(s)
+    JSON.parse(s)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :femida do
-      resources :okb,     only: :show
+      resources :okb,     only: :create
       resources :debtors, only: :show
       resources :fsin,    only: :show
       resources :egrul,   only: :show
