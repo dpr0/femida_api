b3cff921874d10e926b7fe3440fb5244991254df
dvitvitskiy <dvitvitskiy.pro@gmail.com>
2023-08-11 11:03:33 +0300

    okb

--- app/controllers/api/femida/okb_controller.rb
+++ app/controllers/api/femida/okb_controller.rb
@@ -8,13 +8,11 @@ class Api::Femida::OkbController < ApplicationController
     with_error_handling do
       curl = "/opt/cprocsp/bin/amd64/curl -i -X POST -vvv --cert #{ENV['CERT_SHA1_THUMBPRINT']}:#{ENV['CERT_PASSWORD']} --cert-type CERT_SHA1_HASH_PROP_ID:CERT_SYSTEM_STORE_CURRENT_USER:MY"
       hash = { client_id: ENV['OKB_CLIENT_ID'], client_secret: ENV['OKB_CLIENT_SECRET'], grant_type: 'client_credentials', scope: 'openid' }
-      str = "#{curl} -H Content-Type:application/x-www-form-urlencoded #{hash_to_str(hash, 'd')} #{ENV['OKB_HOST']}auth"
-      auth = parse_json str
+      auth = parse_json "#{curl} -H Content-Type:application/x-www-form-urlencoded #{hash_to_str(hash, 'd')} #{ENV['OKB_HOST']}auth"
       t = Time.now
       json = { applicant: params[:okb], submission: { identifier: "#{t.to_i}", date: "#{t.to_date}", app_date: "#{t.to_date}" } }.to_json
       hash = { 'Content-Type:': 'application/json', 'Authorization:Bearer ': auth['access_token'], 'X-Request-Id:': ENV['OKB_CLIENT_SECRET'] }
-      str = "#{curl} #{hash_to_str(hash, 'H')} -d '#{json}' #{ENV['OKB_HOST']}verify"
-      parse_json str
+      parse_json "#{curl} #{hash_to_str(hash, 'H')} -d '#{json}' #{ENV['OKB_HOST']}verify"
     end
   end
 
@@ -25,11 +23,10 @@ class Api::Femida::OkbController < ApplicationController
   end
 
   def parse_json(str)
-    Rails.logger.info('REQUEST: ========================================')
-    Rails.logger.info(str)
-    s = '{' + `#{str}`.split("{")[1..-1].join
-    Rails.logger.info('RESPONSE: =======================================')
-    Rails.logger.info(s)
+    Rails.logger.info("REQUEST: ============ #{str}")
+    s = "{#{`#{str}`.split('{')[1..].join}"
+    Rails.logger.info("RESPONSE: =========== #{s}")
     JSON.parse(s)
   end
 end
+
