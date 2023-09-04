c36f129f3e9d39aedd3b71b37c1adcf7d2e900f8
dvitvitskiy <dvitvitskiy.pro@gmail.com>
2023-09-04 21:48:09 +0300

    job

--- app/jobs/csv_parser_inn_job.rb
+++ app/jobs/csv_parser_inn_job.rb
@@ -4,24 +4,24 @@ class CsvParserInnJob < ApplicationJob
   def perform(id)
     parser = CsvParser.find_by(file_id: id)
     parser.update(status: 6)
-    CsvUser.where(file_id: id, is_phone_verified: nil).in_batches(of: 100).each do |batch|
+    CsvUser.where(file_id: id, is_passport_verified: [nil, false]).in_batches(of: 100).each do |batch|
       array = []
       batch.each do |u|
         is_passport_verified = u.is_passport_verified
         is_passport_verified ||= begin
-                                   inn = InnService.call(
-                                     passport: u.passport,
-                                     date: u.birth_date,
-                                     f: u.last_name&.downcase,
-                                     i: u.first_name&.downcase,
-                                     o: u.middle_name&.downcase
-                                   )
-                                   z = inn && inn['inn'].present?
-                                   is_passport_verified_source = :inn_service if z
-                                   z
-                                 rescue
-                                   false
-                                 end
+        inn = InnService.call(
+            passport: u.passport,
+            date: u.birth_date,
+            f: u.last_name&.downcase,
+            i: u.first_name&.downcase,
+            o: u.middle_name&.downcase
+          )
+          z = inn && inn['inn'].present?
+          is_passport_verified_source = :inn_service if z
+          z
+        rescue
+          false
+        end
 
         zx = {
           id: u.id,
