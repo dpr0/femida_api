ea5c87ad79487b14a31e91b0b5afc0f08ca0e64c
dvitvitskiy <dvitvitskiy.pro@gmail.com>
2023-09-11 11:07:51 +0300

    fix

--- app/jobs/csv_parser_solar_passp_job.rb
+++ app/jobs/csv_parser_solar_passp_job.rb
@@ -10,36 +10,29 @@ class CsvParserSolarPasspJob < ApplicationJob
       batch.each do |u|
         is_phone_verified = u.is_phone_verified
         is_passport_verified = u.is_passport_verified
-        is_passport_verified ||= begin
-          resp = person_service.search(u.slice(%i[first_name last_name middle_name birth_date]))
-          if resp && resp['count'] > 0
-            inform = resp['data'].map { |data| JSON.parse(data['Information'])['D'] }
-            tels = inform.select { |x| x.join(' ').include?('ТЕЛЕФОН') }.compact.select do |x|
-              ['СВЯЗЬ', 'ТЕЛЕФОН ЮЛ', 'ТЕЛЕФОН РАБОТЫ'].map { |xx| xx if x.join(' ').include? xx }.compact.blank?
-            end.compact
-            is_phone_verified ||= tels.map { |xx| xx.join(' ').scan(/[0-9]{10,11}/).map { |x| x.last(10) }.select { |x| x.first == '9' } }.flatten.include? u.phone
-            is_phone_verified ||= resp['data'].find do |d|
-              d['LastName']&.downcase == u.last_name && d['FirstName']&.downcase == u.first_name && d['Telephone']&.last(10) == u.phone.last(10)
-            end.present?
-            z = resp['data'].select { |d| d['Passport'] == u.passport }.present?
-            z ||= inform.select { |x| x.join(' ').include? 'ПАСПОРТ' }.compact.map { |xx| xx.join(' ').scan(/[0-9]{10}/) }.flatten.include? u.passport
-            z
-          end
-        rescue
-          false
+        resp = person_service.search(u.slice(%i[first_name last_name middle_name birth_date]))
+        if resp && resp['count'] > 0
+          is_phone_verified ||= resp['data'].find do |d|
+            tels = %w[Телефон_сотовый Связь_с_телефоном Телефон_работы ТЕЛЕФОН].map { |x| d[x].scan(/\d/).join.last(10) if d[x].present? }.compact.uniq
+            name = d['ИМЯ'].downcase
+            name&.include?(u.last_name) && name&.include?(u.first_name) && tels.include?(u.phone.last(10))
+          end.present?
+          is_passport_verified ||= resp['data'].find { |x| x['ПАСПОРТ'] == u.passport }.present?
         end
 
-        zx = {
-          id: u.id,
-          is_passport_verified: is_passport_verified || false,
-          is_phone_verified: is_phone_verified || false,
-          is_phone_verified_source: u.is_phone_verified ? u.is_phone_verified_source : (:solar if is_phone_verified),
-          is_passport_verified_source: u.is_passport_verified ? u.is_passport_verified_source : (:solar if is_passport_verified)
-        }
-        Rails.logger.info(zx)
-        array << zx
+        if is_phone_verified || is_passport_verified
+          zx = {
+            id: u.id,
+            is_passport_verified: is_passport_verified || false,
+            is_phone_verified: is_phone_verified || false,
+            is_phone_verified_source: u.is_phone_verified ? u.is_phone_verified_source : (:solar if is_phone_verified),
+            is_passport_verified_source: u.is_passport_verified ? u.is_passport_verified_source : (:solar if is_passport_verified)
+          }
+          Rails.logger.info(zx)
+          array << zx
+        end
       end
-      CsvUser.upsert_all(array, update_only: %i[is_passport_verified is_phone_verified is_phone_verified_source is_passport_verified_source])
+      CsvUser.upsert_all(array, update_only: %i[is_passport_verified is_phone_verified is_phone_verified_source is_passport_verified_source]) if array.present?
     end
 
     CsvParser.find_by(file_id: id).update(
