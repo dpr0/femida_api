23e860151db6d7a1901f70e5750a2c41097ab668
dvitvitskiy <dvitvitskiy.pro@gmail.com>
2023-09-12 13:23:11 +0300

    fix

--- app/services/person_service.rb
+++ app/services/person_service.rb
@@ -21,8 +21,8 @@ class PersonService
 
   def post_req(hash, path, headers = {})
     resp = RestClient.post("#{ENV['FEMIDA_PERSONS_API_HOST']}/api/#{path}", hash, headers)
-    resp.code == 200 ? JSON.parse(resp.body) : {}
+    resp.code == 200 ? JSON.parse(resp.body) : { 'data' => [] }
   rescue
-    {}
+    { 'data' => [] }
   end
 end
