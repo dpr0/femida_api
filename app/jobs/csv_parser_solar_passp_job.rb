7183327dff3ef7bea92a4c8a9320525525f5818f
dvitvitskiy <dvitvitskiy.pro@gmail.com>
2023-09-12 12:47:14 +0300

    fix

--- app/jobs/csv_parser_solar_passp_job.rb
+++ app/jobs/csv_parser_solar_passp_job.rb
@@ -9,26 +9,26 @@ class CsvParserSolarPasspJob < ApplicationJob
       .in_batches(of: 100).each do |batch|
       array = []
       batch.each do |u|
+        zx = { id: u.id }
         is_phone_verified = u.is_phone_verified
         is_passport_verified = u.is_passport_verified
         resp = person_service.search(u.slice(%i[first_name last_name middle_name birth_date]))
         if resp && resp['count'] && resp['count'] > 0
-          is_phone_verified ||= resp['data'].find do |d|
-            tels = %w[Телефон_сотовый Связь_с_телефоном Телефон_работы ТЕЛЕФОН].map { |x| d[x].scan(/\d/).join.last(10) if d[x].present? }.compact.uniq
-            name = d['ИМЯ']&.downcase&.tr('ё', 'е')&.split(' ')
-            name&.include?(u.last_name) && name&.include?(u.first_name) && tels.include?(u.phone.last(10))
-          end.present?
-          is_passport_verified ||= resp['data'].find { |x| x['ПАСПОРТ'] == u.passport }.present?
+          unless is_phone_verified
+            r = resp['data'].find do |d|
+              tels = %w[Телефон_сотовый Связь_с_телефоном Телефон_работы ТЕЛЕФОН].map { |x| d[x].scan(/\d/).join.last(10) if d[x].present? }.compact.uniq
+              name = d['ИМЯ']&.downcase&.tr('ё', 'е')&.split(' ')
+              name&.include?(u.last_name) && name&.include?(u.first_name) && tels.include?(u.phone.last(10))
+            end.present?
+            zx.merge(is_phone_verified: true, is_phone_verified_source: :solar) if r
+          end
+          unless is_passport_verified
+            r = resp['data'].find { |x| [x['ПАСПОРТ'], x['ПАСПОРТ_']].compact.include? u.passport }.present?
+            zx.merge(is_passport_verified: true, is_passport_verified_source: :solar) if r
+          end
         end
 
-        if is_phone_verified || is_passport_verified
-          zx = {
-            id: u.id,
-            is_phone_verified: is_phone_verified || false,
-            is_passport_verified: is_passport_verified || false,
-            is_phone_verified_source: u.is_phone_verified ? u.is_phone_verified_source : (:solar if is_phone_verified),
-            is_passport_verified_source: u.is_passport_verified ? u.is_passport_verified_source : (:solar if is_passport_verified)
-          }
+        if zx[:is_phone_verified] || zx[:is_passport_verified]
           Rails.logger.info(zx)
           array << zx
         end
