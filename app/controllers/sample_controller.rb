d3ea31b8be3d385b2f8b615250ceee5aadd7ecfd
dvitvitskiy <dvitvitskiy.pro@gmail.com>
2023-09-13 14:09:01 +0300

    fix

--- app/controllers/sample_controller.rb
+++ app/controllers/sample_controller.rb
@@ -8,13 +8,21 @@ class SampleController < ApplicationController
   def index
     user = params['csv_user'].permit!.to_h.reject { |_, value| value.blank? } if params['csv_user'].present?
     @csv_user = CsvUser.new
-    @results = user ? PersonService.instance.search(user)['data'] : []
     @requests_results = user ? Request.where(user) : []
     @parsed_user_results = user ? ParsedUser.where(user) : []
+    if user
+      limit = params.delete(:limit)
+      offset = params.delete(:offset)
+      user.merge!(limit: limit) if limit
+      user.merge!(offset: offset) if offset
+      response = PersonService.instance.search(user)
+      @results = response['data']
+    end
+
     if user && (@results.present? || @requests_results.present? || @parsed_user_results.present?)
       pasp1 = @results.map { |x| x['ПАСПОРТ'] }.compact.uniq
       pasp2 = @parsed_user_results.map(&:passport).compact.uniq
-      @inn = (pasp1 + pasp2).uniq.map do |pasp|
+      @inn = (pasp1 + pasp2).uniq.first(5).map do |pasp|
         if user['last_name'] && user['first_name'] && user['middle_name'] && user['birth_date']
           inn = InnService.call(
             f: user['last_name'],
