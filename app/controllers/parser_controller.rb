8d3c76d188122ada20d6823492b4d5b25c3aa016
dvitvitskiy <dvitvitskiy.pro@gmail.com>
2023-09-14 18:31:22 +0300

    fix

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -82,7 +82,7 @@ class ParserController < ApplicationController
   def add_score
     errors = []
     array = []
-    file = ActiveStorage::Attachment.find_by(id: 21)
+    file = ActiveStorage::Attachment.find_by(id: 22)
     csv_users = CsvUser.where(file_id: params[:parser_id]).to_a
 
     file.open do |f|
@@ -90,7 +90,7 @@ class ParserController < ApplicationController
         line = f.readline.force_encoding('UTF-8').chomp.split(';')
         next if line[0] == 'external_id' || line[0].blank?
 
-        u = csv_users.select { |u| u.phone == line[1] && u.passport == line[2] }
+        u = csv_users.find { |u| u.phone == line[1] && u.passport == line[2] }
         if line[9].present? && u.present?
           score = line[9].tr(',','.').to_f
           Rails.logger.info i
